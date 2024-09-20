628f2b076a5dbb1713a3db1288d89007370c9bbc
대원씨앤씨 정민규 <ck03412@naver.com>
2024-09-20 09:51:50 +0900

    관심사의 분리

--- src/main/java/tobyspring/hellospring/payment/service/PaymentService.java
+++ src/main/java/tobyspring/hellospring/payment/service/PaymentService.java
@@ -1,7 +1,6 @@
 package tobyspring.hellospring.payment.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cglib.core.Local;
 import tobyspring.hellospring.payment.dao.ExRateDate;
 import tobyspring.hellospring.payment.dao.Payment;
 
@@ -11,13 +10,21 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
+        BigDecimal exRate = getExRate(currency);
+
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -26,15 +33,7 @@ public class PaymentService {
 
         ObjectMapper mapper = new ObjectMapper();
         ExRateDate data = mapper.readValue(response, ExRateDate.class);
-        BigDecimal exRate = data.rates().get("KRW");
-
-        // 금액 계산
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return data.rates().get("KRW");
     }
 
     public static void main(String[] args) throws IOException {
