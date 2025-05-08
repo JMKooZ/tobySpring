380f2574517285fd857a2797dfeb144b17ea812c
MK <ck03412@naver.com>
2025-05-08 14:51:23 +0900

    Junit test 작성.

--- src/main/java/tobyspring/hellospring/payment/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/payment/ObjectFactory.java
@@ -1,15 +1,19 @@
 package tobyspring.hellospring.payment;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import tobyspring.hellospring.payment.service.ExRateProvider;
 import tobyspring.hellospring.payment.service.PaymentService;
+import tobyspring.hellospring.payment.service.SimpleExRateProvider;
 import tobyspring.hellospring.payment.service.WebApiExRateProvdier;
-
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
-
+    @Bean
     public ExRateProvider exRateProvider(){
-        return new WebApiExRateProvdier();
+        return new SimpleExRateProvider();
     }
 }
