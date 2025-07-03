c3269d441ef2ec58cad7f46db0405a2a69ed3ac7
대원씨앤씨 정민규 <ck03412@naver.com>
2025-07-03 15:04:54 +0900

    1

--- src/main/java/tobyspring/hellospring/payment/service/PaymentService.java
+++ src/main/java/tobyspring/hellospring/payment/service/PaymentService.java
@@ -2,22 +2,22 @@ package tobyspring.hellospring.payment.service;
 
 import tobyspring.hellospring.payment.dao.Payment;
 
-import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 public class PaymentService {
     private final ExRateProvider exRateProvdier;
+    private final Clock clock;
 
-    public PaymentService(ExRateProvider exRateProvdier) {
+    public PaymentService(ExRateProvider exRateProvdier, Clock clock) {
         this.exRateProvdier = exRateProvdier;
+        this.clock = clock;
     }
 
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
         BigDecimal exRate = exRateProvdier.getExRate(currency);
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return Payment.createdPrepared(orderId, currency, foreignCurrencyAmount, exRate, LocalDateTime.now(clock));
     }
 }
--- src/main/java/tobyspring/hellospring/payment/service/WebApiExRateProvdier.java
+++ src/main/java/tobyspring/hellospring/payment/service/WebApiExRateProvdier.java
@@ -1,5 +1,6 @@
 package tobyspring.hellospring.payment.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import tobyspring.hellospring.payment.dao.ExRateDate;
 
@@ -8,20 +9,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 
-public class WebApiExRateProvdier implements ExRateProvider{
+public class WebApiExRateProvdier implements ExRateProvider {
     @Override
-    public BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
+    public BigDecimal getExRate(String currency) {
+        String url = "https://open.er-api.com/v6/latest/" + currency;
+        URI uri;
+        String response;
+        HttpURLConnection connection;
+        ExRateDate data;
 
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateDate data = mapper.readValue(response, ExRateDate.class);
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = (HttpURLConnection) uri.toURL().openConnection();
+            // finally 대신 블록안에서 자동으로 리소스를 제거해주는 방법 try(변수 선언부){} -> br.close() 자동 닫힘 (AutoCloseable)
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                response = br.lines().collect(Collectors.joining());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            data = mapper.readValue(response, ExRateDate.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return data.rates().get("KRW");
     }
 }
