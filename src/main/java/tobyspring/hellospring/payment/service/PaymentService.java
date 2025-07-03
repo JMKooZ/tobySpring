0802768762e720a70b0378bf4f07e31b590da938
대원씨앤씨 정민규 <ck03412@naver.com>
2025-07-03 14:35:23 +0900

    Reapply "1"
    This reverts commit 62cf9a3a315cdaa3123e115f73cd57631c0f3e00.

--- src/main/java/tobyspring/hellospring/payment/service/PaymentService.java
+++ src/main/java/tobyspring/hellospring/payment/service/PaymentService.java
@@ -2,7 +2,6 @@ package tobyspring.hellospring.payment.service;
 
 import tobyspring.hellospring.payment.dao.Payment;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.LocalDateTime;
@@ -16,7 +15,7 @@ public class PaymentService {
         this.clock = clock;
     }
 
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
         BigDecimal exRate = exRateProvdier.getExRate(currency);
 
         return Payment.createdPrepared(orderId, currency, foreignCurrencyAmount, exRate, LocalDateTime.now(clock));
